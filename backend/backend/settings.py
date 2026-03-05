b88ad52d953ed21d42ae0ad199ede7c64d315bc0
Ubuntu <ubuntu@fv4ru0n61577i8h3j58b.auto.internal>
2026-03-05 19:02:52 +0000

    commit rewiev

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -5,9 +5,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '158.160.134.177', 'styletesso.ddns.net']
 
 
 # Application definition
@@ -104,6 +104,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static_backend'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
